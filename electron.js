ba4636a5480e847c0655da15a780989d3f350d1a
Ryan McCue <me@ryanmccue.info>
2017-05-25 14:41:46 +1000

    Add and configure electron-updater

--- electron.js
+++ electron.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, Menu, protocol, shell } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, protocol, shell } = require('electron');
+const { autoUpdater } = require("electron-updater");
 const path = require('path');
 
 if (process.env.NODE_ENV === 'development') {
@@ -84,6 +85,49 @@ function createAboutWindow() {
 	})
 }
 
+function sendEvent( event, data) {
+	if ( aboutWindow ) {
+		aboutWindow.webContents.send( event, data );
+	}
+	if ( win ) {
+		win.webContents.send( event, data );
+	}
+}
+
+function configureAutoupdates() {
+	const sendStatusToWindow = data => sendEvent( 'update-status', data );
+
+	// Configure the updater.
+	autoUpdater.allowPrerelease = true;
+
+	// Listen to events.
+	autoUpdater.on( 'checking-for-update', () => {
+		sendStatusToWindow( { type: 'checking-for-update' } )
+	})
+	autoUpdater.on( 'update-available', info => {
+		sendStatusToWindow( { type: 'update-available', info } )
+	})
+	autoUpdater.on( 'update-not-available', (ev, info) => {
+		console.log( ev, info );
+		sendStatusToWindow( { type: 'update-not-available', info } )
+	})
+	autoUpdater.on( 'error', (ev, err) => {
+		sendStatusToWindow( { type: 'error', err } )
+	})
+	autoUpdater.on( 'update-downloaded', (ev, info) => {
+		sendStatusToWindow( { type: 'update-downloaded', info } )
+	})
+	// Not supported on Mac:
+	// https://github.com/electron-userland/electron-builder/issues/1167
+	// https://github.com/Squirrel/Squirrel.Mac/issues/200
+	autoUpdater.on( 'download-progress', (ev, progressObj) => {
+		sendStatusToWindow( { type: 'download-progress', progressObj } )
+	})
+
+	ipcMain.on( 'check-update', () => autoUpdater.checkForUpdates() )
+	ipcMain.on( 'install-update', () => autoUpdater.quitAndInstall() )
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -103,6 +147,7 @@ app.on('ready', () => {
 	);
 
 	createWindow();
+	configureAutoupdates();
 
 	const mainMenu = Menu.buildFromTemplate([
 		{
