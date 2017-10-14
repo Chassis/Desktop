e0148a6db4ba62f1e44d4f2580b2e9b697db9b16
Joe McGill <joemcgill@gmail.com>
2017-10-14 15:17:39 +0200

    Add GitHub login button to welcome screen

--- src/Installer/Welcome.js
+++ src/Installer/Welcome.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from '../Button';
 import Step from '../Step';
 
+
 export default props => <Step>
 	<header>
 		<h1>Welcome to Chassis Desktop</h1>
@@ -12,9 +13,20 @@ export default props => <Step>
 	<p>Welcome to Chassis Desktop. To help you get set up, we're going to run through some installation checks and make sure everything's installed.</p>
 	<p>
 		<Button
+			icon="github"
+			light
+			shortcut="Cmd+L"
+			onClick={ () => console.log('Worked.') }
+		>
+		 Connect
+		</Button>
+		<Button
 			icon="arrow-right"
 			light
+			noborder
 			onClick={ props.onNext }
-		>Get Started!</Button>
+		>
+			Skip for now
+		</Button>
 	</p>
 </Step>;
